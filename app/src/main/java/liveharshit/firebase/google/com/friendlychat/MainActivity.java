4e710dab633584f00601d7fd05fbc2ea3cbd3d5b
liveHarshit <live.harshit@hotmail.com>
2018-06-16 13:32:20 +0530

    upload image to firebase storage

--- app/src/main/java/liveharshit/firebase/google/com/friendlychat/MainActivity.java
+++ app/src/main/java/liveharshit/firebase/google/com/friendlychat/MainActivity.java
@@ -2,6 +2,7 @@ package liveharshit.firebase.google.com.friendlychat;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.support.annotation.NonNull;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -24,6 +26,8 @@ import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -31,19 +35,22 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
-    public static final int RC_SIGN_IN =1;
-
+    public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER =  2;
+    private static final String TAG = "MainActivity";
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
     private ProgressBar mProgressBar;
@@ -55,8 +62,9 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener childEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference chatPhotosStorageReference;
     private String mUsername;
-
 
 
     @Override
@@ -68,7 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
         messageDatabaseReference = firebaseDatabase.getReference().child("messages");
+        chatPhotosStorageReference = firebaseStorage.getReference().child("chat_photos");
+
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -89,7 +100,11 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.e("PhotoButton clicked","Yes");
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
             }
         });
 
@@ -118,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(),mUsername,null);
+                FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(), mUsername, null);
                 messageDatabaseReference.push().setValue(friendlyMessage);
 
                 // Clear input box
@@ -127,14 +142,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = mFirebaseAuth.getCurrentUser();
-                if(user!=null) {
+                if (user != null) {
                     onSignedInInitialized(user.getDisplayName());
-                    } else  {
+                } else {
                     onSignedOutCleanUp();
                     startActivityForResult(
                             AuthUI.getInstance()
@@ -154,16 +168,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RC_SIGN_IN) {
-            if(resultCode == RESULT_OK) {
+        Log.e("method Called", "Yes");
+        Log.e("Request code", Integer.toString(requestCode));
+        Log.e("Result code", Integer.toString(resultCode));
+        Log.e("Actual result code", Integer.toString(RESULT_OK));
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
-            } else if(resultCode == RESULT_CANCELED) {
+            } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Signed in canceled", Toast.LENGTH_SHORT).show();
                 finish();
             }
-        }
-    }
+        } if(requestCode == RC_PHOTO_PICKER&&resultCode == RESULT_OK ) {
 
+            Uri selectedImageUri = data.getData();
+
+            // Get a reference to store file at chat_photos/<FILENAME>
+            final StorageReference photoRef = chatPhotosStorageReference.child(selectedImageUri.getLastPathSegment());
+
+            photoRef.putFile(selectedImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    photoRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Uri downloadUrl = uri;
+                            Log.e("Download url", downloadUrl.toString());
+                            FriendlyMessage friendlyMessage = new FriendlyMessage(null, mUsername, downloadUrl.toString());
+                            messageDatabaseReference.push().setValue(friendlyMessage);
+                        }
+                    });
+
+
+                }
+            });
+        }
+
+
+    }
 
 
     @Override
@@ -189,7 +231,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(mAuthStateListener==null) {
+        if (mAuthStateListener == null) {
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
         }
         mMessageAdapter.clear();
@@ -207,7 +249,7 @@ public class MainActivity extends AppCompatActivity {
         attachDatabaseReadListener();
     }
 
-    private void onSignedOutCleanUp () {
+    private void onSignedOutCleanUp() {
         mUsername = ANONYMOUS;
         mMessageAdapter.clear();
         detachDatabaseReadListener();
@@ -247,7 +289,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void detachDatabaseReadListener() {
-        if(childEventListener!=null) {
+        if (childEventListener != null) {
             messageDatabaseReference.removeEventListener(childEventListener);
             childEventListener = null;
         }
